a3d856ef243eaa9f274324c3dd8e79eeae1b152b
Ubuntu <ubuntu@ip-172-31-23-238.ap-southeast-1.compute.internal>
2026-06-03 06:09:02 +0000

    create the Main Architecture File

---
+++ modules/vpc/main.tf
@@ -0,0 +1,43 @@
+# 1. The Core VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+# 2. The Internet Gateway (Allows public traffic in/out)
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+# 3. Public Subnets (We use 'count' to loop through our list of IPs)
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true # Gives servers here a public IP automatically
+
+  tags = {
+    Name = "${var.environment}-public-subnet-${count.index + 1}"
+  }
+}
+
+# 4. Private Subnets (No public IPs allowed here)
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.environment}-private-subnet-${count.index + 1}"
+  }
+}
