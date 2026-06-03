056b48a3dbc3dd01488a44cec7539182cdfefb85
Ubuntu <ubuntu@ip-172-31-23-238.ap-southeast-1.compute.internal>
2026-06-03 06:49:35 +0000

    fixing route table and associate the router table with public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -41,3 +41,24 @@ resource "aws_subnet" "private" {
     Name = "${var.environment}-private-subnet-${count.index + 1}"
   }
 }
+
+# 5. Public Route Table (The GPS pointing to the internet)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+# 6. Associate the Route Table with the Public Subnets
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
