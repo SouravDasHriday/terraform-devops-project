bcfc1a11ff52359921921e0f553d44e3873dbab2
Ubuntu <ubuntu@ip-172-31-23-238.ap-southeast-1.compute.internal>
2026-06-03 03:40:21 +0000

    The Memory Layer (Storing terraform state into s3 and state lock with DynamoBD).

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1" # Select my region
+}
+
+# Create the S3 Bucket for Remote State:
+resource "aws_s3_bucket" "terraform_state" {
+  # CRITICAL: Change "devops-project-tfstate-sourav-2026" Set unique name so it is globally unique!
+  bucket        = "devops-project-tfstate-sourav-2026" 
+  force_destroy = true
+}
+
+# Enable versioning so we keep a history of our infrastructure state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Create the DynamoDB Table for State Locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "devops-project-tfstate-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID" # This attribute name is strictly required by Terraform!
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
